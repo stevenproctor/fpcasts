82ed6a2f5c9b34d707eca1424932014bc77eb94c
Proctor <steven.proctor@gmail.com>
2016-09-12 14:34:04 -0500

    create local copy of images

--- fpcasts.rb
+++ fpcasts.rb
@@ -6,15 +6,27 @@ require 'rss'
 require 'yaml'
 require 'ostruct'
 
-def feed_summary(feed_url)
+
+def get_image_path!(rss, output_directory)
+  if (rss.channel.itunes_image)
+    image_location = rss.channel.itunes_image.href
+  else
+    image_location = rss.image.url
+  end
+
+  image_path = File.basename(image_location)
+
+  `wget -O - #{image_location} > #{File.join(output_directory, image_path)}`
+
+  image_path
+end
+
+def feed_summary(feed_url, output_directory)
   puts "[#{Time.now}] generating summary for #{feed_url}"
   rss = RSS::Parser.parse(feed_url, false)
   latest = rss.items.first
-  if (rss.channel.itunes_image)
-    image = rss.channel.itunes_image.href
-  else
-    image = rss.image.url
-  end
+  image = get_image_path!(rss, output_directory)
+
   OpenStruct.new({
     url: feed_url,
     title: CGI.escapeHTML(rss.channel.title),
@@ -28,22 +40,27 @@ rescue
   STDERR.puts "error generating summary for feed: #{feed_url}"
 end
 
-def get_feed_summaries(feed_config)
+def get_feed_summaries(feed_config, output_directory)
   feed_urls = YAML.load(File.read(feed_config))
-  feed_urls.map{ |feed_url| feed_summary(feed_url) }.sort_by{ |feed| feed.title.downcase }
+  feed_urls.map{ |feed_url| feed_summary(feed_url, output_directory) }.sort_by{ |feed| feed.title.downcase }
 end
 
-def main(config, template_path, output_path)
+def main(config, template_path, output_directory)
+  `cp ./*.css #{output_directory}`
+
   template = File.read(template_path)
+  output_filename = File.basename(template_path, '.erb')
 
   renderer = ERB.new(template)
 
-  feeds = get_feed_summaries(config)
+  feeds = get_feed_summaries(config, output_directory)
   output = renderer.result(binding)
+
+  output_path = File.join(output_directory, output_filename)
 
   File.open(output_path, 'w') do |f|
     f << output
   end
 end
 
-main('config/feeds.yml', 'templates/index.html.erb', 'output/index.html')
+main('config/feeds.yml', 'templates/index.html.erb', 'output')
